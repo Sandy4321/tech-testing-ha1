e1582832d13ac05a4bb612c0dd3bf94624089bd6
abbadoh <abbadoh@mail.ru>
2014-10-02 18:10:02 +0400

    add some tests

--- source/tests/test_lib_utils.py
+++ source/tests/test_lib_utils.py
@@ -1,12 +1,20 @@
 __author__ = 'gumo'
 import unittest
 import mock
+from mock import patch, call
 
-from lib.utils import (create_pidfile, parse_cmd_args, get_tube)
+from tarantool_queue import tarantool_queue
+from multiprocessing import Process
+from random import randrange
+
+from lib.utils import *
+
+
+RANDOM_INT_OFFSET = 10000
 
 class LibUtilsTestCase(unittest.TestCase):
     def test_create_pidfile_example(self):
-        pid = 42
+        pid = randrange(RANDOM_INT_OFFSET)
         m_open = mock.mock_open()
         with mock.patch('lib.utils.open', m_open, create=True):
             with mock.patch('os.getpid', mock.Mock(return_value=pid)):
@@ -15,7 +23,7 @@ class LibUtilsTestCase(unittest.TestCase):
         m_open.assert_called_once_with('/file/path', 'w')
         m_open().write.assert_called_once_with(str(pid))
 
-    def test_parse_cmd_args(self):
+    def test_parse_cmd_args_1(self):
         cfg = '/test'
         pid = '/pid'
         args = parse_cmd_args(['-c', cfg, '-P', pid], 'test')
@@ -23,14 +31,25 @@ class LibUtilsTestCase(unittest.TestCase):
         self.assertTrue(args.pidfile == pid)
         self.assertFalse(args.daemon)
 
-"""
+
     def test_get_tube(self):
         host = 'host'
-        port = 'port'
-        space = 'space'
+        port = randrange(RANDOM_INT_OFFSET)
+        space = randrange(RANDOM_INT_OFFSET)
         name = 'name'
-        with mock.patch('tarantool_queue.Queue', 'tube', return_value='') as mock_queue:
+        with patch.object(tarantool_queue.Queue, 'tube', return_value=None) as mock_queue:
             get_tube(host, port, space, name)
         mock_queue.assert_called_once_with(name)
-"""
 
+    def test_spawn_workers(self):
+        num = randrange(RANDOM_INT_OFFSET)
+        target = 'target'
+        args = ['', '']
+        parent_id = 1
+        with patch.object(Process, 'start', return_value=None) as mock_process:
+            spawn_workers(num, target, args, parent_id)
+        for _ in range(0, num, 1):
+            calls = []
+            calls.append(call())
+        mock_process.assert_has_calls(calls)
+        mock_process.asert_called_with()
--- source/tests/test_notification_pusher.py
+++ source/tests/test_notification_pusher.py
@@ -1,5 +1,9 @@
 import unittest
-
+# import mock
+# from mock import patch
+#
+# from notification_pusher import *
 
 class NotificationPusherTestCase(unittest.TestCase):
-    pass
+    def test_notification_worker(self):
+        pass
