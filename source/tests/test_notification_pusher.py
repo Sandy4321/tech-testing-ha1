580ffb8e32c095d635871fbc89689190304af6cb
Oleg <oleg@Oleg-Mac.local>
2014-10-03 13:05:00 +0400

    some np tests

--- source/tests/test_notification_pusher.py
+++ source/tests/test_notification_pusher.py
@@ -1,7 +1,6 @@
 import unittest
 import mock
-from notification_pusher import create_pidfile
-
+from notification_pusher import *
 
 class NotificationPusherTestCase(unittest.TestCase):
     def test_create_pidfile_example(self):
@@ -13,3 +12,30 @@ class NotificationPusherTestCase(unittest.TestCase):
 
         m_open.assert_called_once_with('/file/path', 'w')
         m_open().write.assert_called_once_with(str(pid))
+
+    def test_daemonize(self):
+        fork_mock = mock.Mock(return_value=0)
+        with mock.patch('os.fork', fork_mock):
+            with mock.patch('os.setsid', mock.Mock(), create=True):
+                daemonize()
+        fork_mock.assert_called_with()
+
+    def test_parse_cmd_args(self):
+        args = '-c /conf -P /pidfile -d'
+        args = args.split(' ')
+        parsed_args = parse_cmd_args(args)
+        self.assertEqual(parsed_args.daemon, True)
+        self.assertEqual(parsed_args.config, '/conf')
+        self.assertEqual(parsed_args.pidfile, '/pidfile')
+
+    def test_load_config_from_pyfile(self):
+        def mocked_execfile(filepath, variables):
+            variables = {
+                'a': 23,
+                'death': 42
+            }
+            pass
+
+        with mock.patch('__builtin__.execfile', mocked_execfile):
+            cfg = load_config_from_pyfile('/test')
+        self.assertEqual(cfg.a, 23)
